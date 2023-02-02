d1d15be586951d85022e3a752d9002b7b4b5cd20
gfouz <gfouz1975@gmail.com>
2023-02-02 02:44:35 -0500

    fixing some errors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/next-blog-2023',
+  assetPrefix: '/next-blog-2023',
   compiler: {
     styledComponents: true,
   }
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -7,7 +7,6 @@ import matter from "gray-matter";
 import ReactMarkdown from "react-markdown";
 import styled from "styled-components";
 
-import sidebarPicture from '../../../public/images/gfouz.jpg';
 import { attached_1, attached_2 } from 'constants/constants';
 
 
